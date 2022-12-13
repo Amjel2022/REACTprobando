bbeea2f21b8e64f32bc4366c8066186094633422
Amjel2022 <amjel105@gmail.com>
2022-12-13 20:01:50 -0300

    Mis primeros componentes :)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import Title from './componentes/Title';
+import Mascota from './componentes/Mascota';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Title />
+      <Mascota />
+      {/* Los componentes nativos del REACT son como los de HTML, para diferenciar escribo las etiquetas que quiero declarar en mayúsculas.  */}
       <header className="App-header">
         
         <img src="images/perritoTierno1.jpg" alt="Perrito" />
         
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -26,3 +31,5 @@ function App() {
 }
 
 export default App;
+
+
---
+++ src/componentes/Title.js
@@ -0,0 +1,10 @@
+// los componentes está bueno que tengan los nombres con inicio en mayúsculas //
+// Exportamos nuestro componente
+//Los componentes son funciones
+import './Title.css';
+
+export default function Title () {
+    return (
+        <h1 className="title">Hola, te hablo desde el componente</h1>
+    )
+} 

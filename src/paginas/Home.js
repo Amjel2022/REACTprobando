a718bc26b3ff4f818273660e2f6852529cf48acb
Amjel2022 <amjel105@gmail.com>
2022-12-29 11:11:22 -0300

    Ejemplo useState (aparecer y desaparecer boton)

--- src/paginas/Home.js
+++ src/paginas/Home.js
@@ -1,10 +1,12 @@
 import CardWrapper from "../componentes/CardWrapper"
 import EjemploEvento from "../componentes/EjemploEvento"
+import EjemploState from "../componentes/EjemploState"
 
 export default function Home () {
     return (
         <> 
             <h1>Estás en la home</h1>
+            <EjemploState />
             <EjemploEvento />
             <CardWrapper 
                 titulo={'Ofertas'}
